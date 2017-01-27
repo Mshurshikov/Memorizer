2eaa20f32bb5b308cc6b0b128f784bc1c1ec92b4
uxeab <uxeab@student.kit.edu>
2017-01-27 11:28:15 +0100

    Basic button layout created

--- memorizer.py
+++ memorizer.py
@@ -3,7 +3,24 @@ import sys
 from random import shuffle, random
 from tkinter import *
 
+buttons = []
+
+SIDE = 6
+QELEMENTS = SIDE ** 2 // 2
+
+def cmd():
+	print ("Button pressed")
+
+
+
 main_window = Tk()
 
-main_window.mainloop()
+for i in range(SIDE):
+	buttons.append([])
+	for j in range(SIDE):
+		b = Button (text = 'x',
+					command = cmd)
+		buttons[i].append(b)
+		b.grid(row = i, column = j)
 
+main_window.mainloop()
