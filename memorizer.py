d27ee7ad5e3b805cdf07051fc3b38dbe72882666
uxeab <uxeab@student.kit.edu>
2017-01-28 13:44:58 +0100

    Opened images counter added

--- memorizer.py
+++ memorizer.py
@@ -10,21 +10,26 @@ buttons = []
 numbers = []
 main_window = Tk()
 
+opened_images = 0
+left_images = QELEMENTS
 
 prev = None
 
 def show(btns, nums, i, j):
 	global prev
+	global opened_images
+	global left_images
 	#btns[i][j].configure(text = nums[i*SIDE + j])
 	btns[i][j].configure(image = images[i*SIDE + j])
-	#print(btns[prev[0]][prev[1]].grid_info())
-	#print(btns[i][j].grid_info())
 	if prev:
-		coordinates = btns[i][j].grid_info()
-		prev_coordinates = btns[prev[0]][prev[1]].grid_info()
 		if (nums[prev[0]*SIDE + prev[1]] != nums[i*SIDE + j] 
-			or ((coordinates['column'] == prev_coordinates['column']) and (coordinates['row'] == prev_coordinates['row']))):
+			or ((btns[i][j].grid_info()['column'] == btns[prev[0]][prev[1]].grid_info()['column']) 
+				and (btns[i][j].grid_info()['row'] == btns[prev[0]][prev[1]].grid_info()['row']))):
 			main_window.after(1000, hide, btns, prev, i, j)
+		else:
+			progress_counter.configure(text = "Opened: " + str(opened_images + 1) + " Left: " + str(left_images - 1))
+			opened_images += 1
+			left_images -= 1
 		prev = None
 	else:
 		prev = (i,j)
@@ -58,9 +63,13 @@ for i in range(SIDE):
 	for j in range(SIDE):
 		b = Button (text = numbers[i*SIDE + j],
 					image = images[i*SIDE + j],
+					relief=FLAT,
 					command = lambda ii=i, jj=j:show(buttons,files,ii,jj))
 		buttons[i].append(b)
 		b.grid(row = i, column = j)
+
+progress_counter = Label (text = "Opened: " + str(opened_images) + " Left: " + str(left_images))
+progress_counter.grid(row = SIDE + 1, column = 0, columnspan = SIDE)
 
 main_window.title("Memorizer")
 main_window.after(2000, hide_all, buttons)
