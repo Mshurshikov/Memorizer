3332295b47b0316b302edf91e6126950fb1cf5a0
MShurshikov <m.shurshikov@gmail.com>
2017-02-04 12:44:46 +0100

    image_opened counter and hide_time created

--- model.py
+++ model.py
@@ -6,5 +6,7 @@ class Game(object):
 		self.qelements = side ** 2 // 2
 		self.time = 0
 		self.show_time = 3000 #in ms
+		self.hide_time = 500 #in ms
 		self.player_name = None
+		self.images_opened = 0
 		
